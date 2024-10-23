9c96265380fed3b3c01207bf8e0c5bfc8d109756
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-10-22 20:03:42 -0500

    build: display /new page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("node:path");
 const PORT = process.env.PORT || 3000;
 const indexRouter = require("./routes/indexRouter");
+const newMessageRouter = require("./routes/newMessageRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -11,5 +12,6 @@ const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
 app.use("/", indexRouter);
+app.use("/new", newMessageRouter);
 
 app.listen(PORT);
