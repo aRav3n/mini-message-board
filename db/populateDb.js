25aa0126b62a55e475d3386f8e3d6ac908d37474
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-11-12 10:22:41 -0500

    build: add database interaction files

---
+++ db/populateDb.js
@@ -0,0 +1,36 @@
+const { Client } = require("pg");
+require("dotenv").config();
+const tableName = "messages";
+const dbName = "mini_message_board";
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS ${tableName} (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  text VARCHAR ( 255 ),
+  being VARCHAR ( 255 ),
+  added TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO ${tableName} (text, being)
+VALUES
+  ('May the Force be with you.', 'Luke Skywalker'),
+  ('Never tell me the odds.', 'Han Solo'),
+  ('Help me, Obi-Wan Kenobi. You''re my only hope.', 'Princess Leia'),
+  ('I am your father.', 'Darth Vader'),
+  ('Use the Force, Luke.', 'Obi-Wan Kenobi');
+`;
+
+async function main() {
+  console.log("seeding...");
+  const roleName = process.env.ROLE_NAME;
+  const rolePassword = process.env.ROLE_PASSWORD;
+  const client = new Client({
+    connectionString: `postgresql://${roleName}:${rolePassword}@localhost:5432/${dbName}`,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
